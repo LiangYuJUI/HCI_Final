b7d26a61be2ebdf4f0b876e1fa19e411b3b8d66b
LeoPhoneEgypt <liangyujui@gmail.com>
2023-12-26 17:13:02 +0800

    feat: Merge two pictures

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+import numpy as np
 
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
@@ -10,19 +11,24 @@ pose = mp_pose.Pose(
 
 cap = cv2.VideoCapture(0)
 
+
 while cap.isOpened():
     ret, frame = cap.read()
 
-    try:
-        RGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        results = pose.process(RGB)
+    #try:
+    RGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = pose.process(RGB)
 
-        print(results.pose_landmarks)
-        mp_drawing.draw_landmarks(
-            frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
-        cv2.imshow('Output', frame)
-    except:
-        break
+    print(results.pose_landmarks)
+    mp_drawing.draw_landmarks(
+        frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+    
+    #合併兩組numpy array
+    merge_frame= np.concatenate((frame, RGB), axis= 1)
+    
+    cv2.imshow('Output', merge_frame)
+    #except:
+    #   break
     if cv2.waitKey(1) == ord('q'):
         break
 
