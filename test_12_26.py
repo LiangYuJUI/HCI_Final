1182b832cce171a73e00fbadac957fda4c51395b
chadtied <96424234+chadtied@users.noreply.github.com>
2023-12-26 16:53:45 +0800

    Add files via upload

---
+++ test_12_26.py
@@ -0,0 +1,36 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+pose = mp_pose.Pose(
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5)
+
+cap = cv2.VideoCapture(0)
+
+
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    #try:
+    RGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = pose.process(RGB)
+
+    print(results.pose_landmarks)
+    mp_drawing.draw_landmarks(
+        frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+    
+    #合併兩組numpy array
+    merge_frame= np.concatenate((frame, RGB), axis= 1)
+    
+    cv2.imshow('Output', merge_frame)
+    #except:
+    #   break
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
